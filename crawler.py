a4872f306e09842b7ffd58da4cf4c338e53bb77b
Victor Bona <victor.bona@hotmail.com>
2020-08-16 13:39:22 -0300

    fixing project

--- crawler.py
+++ crawler.py
@@ -4,58 +4,63 @@ import re
 import boto3
 import json
 import os
+import logging
 
-def parsePageContent(content):
-  content = re.sub(r'={1,}.+={1,}', '', content)
-  content = article.split('\n{1,}')
-  return content
+
+def parsePageContent(article):
+    content = re.sub(r'={1,}.+={1,}', '', article)
+    content = content.split('\n{1,}')
+    return content
+
 
 def executeCheckpoint(topics):
-  s3 = boto3.client('s3')
-  file = 'wikipedia-content-dataset.json'
-  bucket = 'wikipedia-crawler-s3'
+    s3 = boto3.client('s3')
+    file = 'wikipedia-content-dataset.json'
+    bucket = 'wikipedia-crawler-s3'
 
-  with open(file, 'w') as outfile:
-    json.dump(topics, outfile)
+    with open(file, 'w') as outfile:
+        json.dump(topics, outfile)
 
-  s3.upload_file(file, bucket, file)
-  os.remove(file)
-  return 0
+    s3.upload_file(file, bucket, file)
+    os.remove(file)
+    return 0
 
-visited_topics = []
-topics = {}
-content_goal = 25000
-checkpoint_treshold = 50
-checkpoint_counter = 0
 
 def crawl():
-  while len(visited_topics) <= content_goal:
-    topic = wikipedia.random(pages=1)
+    visited_topics = []
+    topics = {}
+    content_goal = 25000
+    checkpoint_treshold = 5
+    checkpoint_counter = 0
+    logging.basicConfig(filename='error.log', level=logging.INFO)
 
-    if topic not in visited_topics:
-      try:
-        page = wikipedia.page(topic)
-        if page is not None or page != '':
-          visited_topics.append(topic)
-          print(f'Visiting topic: {topic}')
+    while len(visited_topics) <= content_goal:
+        topic = wikipedia.random(pages=1)
+        if topic not in visited_topics:
+            try:
+                page = wikipedia.page(topic)
+                if page is not None or page != '':
+                    visited_topics.append(topic)
+                    logging.info(f'Visiting topic: {topic}')
+                else:
+                    continue
+
+                article = page.content
+                content = parsePageContent(article)
+                content.append(page.summary)
+                topics[topic] = content
+
+                if checkpoint_counter >= checkpoint_treshold:
+                    checkpoint_counter = executeCheckpoint(topics)
+                    logging.info(f'Checkpoint reached, {len(visited_topics)} visited')
+                else:
+                    checkpoint_counter += 1
+
+                time.sleep(1)
+            except Exception as ex:
+                logging.error(ex)
+                continue
         else:
-          continue
-
-        article = page.content
-        content = parsePageContent(article)
-        content.append(page.summary)
-        topics[topic] = content
-
-        if checkpoint_counter >= checkpoint_treshold:
-          checkpoint_counter = executeCheckpoint(topics)
-          print(f'Checkpoint reached, {len(visited_topics)} visited')
-        else:
-          checkpoint_counter += 1
-
-        time.sleep(1)
-      except:
-        continue
-    else:
-      continue
+            continue
 
 crawl()
